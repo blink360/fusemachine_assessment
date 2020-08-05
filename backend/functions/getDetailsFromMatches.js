d9a77b44ac300001ecd421313bfc04c8024ed597
Pranish Rajbhandari <rajbhandari.pranish@gmail.com>
2020-08-06 00:12:26 +0545

    Added controller for fetching data from OpenFootball and sending response

--- backend/functions/getDetailsFromMatches.js
+++ backend/functions/getDetailsFromMatches.js
@@ -4,11 +4,28 @@ exports.getDetailsFromMatches = (array) => {
         (resolve, reject) => {
             try {
                 //loop through the json arrays and return an object with match details
-                let detailsForTheMatches =[];
+                let detailsForTheMatches = [];
                 array.data.rounds.map((match) => match.matches.forEach((match) => {
-                    detailsForTheMatches.push({data:{
-                        team_one_name: match.team1, team_one_details: { winner: match.score.ft[0] > match.score.ft[1] ? true : false, draw: match.score.ft[0] === match.score.ft[1], goalsFor: match.score.ft[0], goalsAgainst: match.score.ft[1] }, team_two_name: match.team2, team_two_details: { winner: match.score.ft[1] > match.score.ft[0] ? true : false, draw: match.score.ft[0] === match.score.ft[1], goalsFor: match.score.ft[1], goalsAgainst: match.score.ft[0] }
-                    }});
+                    detailsForTheMatches.push({
+                        data: {
+                            team_one_name: match.team1,
+                            team_one_details:
+                            {
+                                winner: match.score.ft[0] > match.score.ft[1] ? true : false,
+                                draw: match.score.ft[0] === match.score.ft[1],
+                                goalsFor: match.score.ft[0],
+                                goalsAgainst: match.score.ft[1]
+                            },
+                            team_two_name: match.team2,
+                            team_two_details:
+                            {
+                                winner: match.score.ft[1] > match.score.ft[0] ? true : false,
+                                draw: match.score.ft[0] === match.score.ft[1],
+                                goalsFor: match.score.ft[1],
+                                goalsAgainst: match.score.ft[0]
+                            }
+                        }
+                    });
                 }));
 
                 resolve(detailsForTheMatches);
