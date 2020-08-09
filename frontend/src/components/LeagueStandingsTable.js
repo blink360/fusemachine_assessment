9f938b2b1786e4b8d2df7330eb5d5f4b349633f7
Pranish Rajbhandari <rajbhandari.pranish@gmail.com>
2020-08-09 07:21:38 +0545

    Added stylings. Resolved unique key warnings while rendering the table

--- frontend/src/components/LeagueStandingsTable.js
+++ frontend/src/components/LeagueStandingsTable.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
-import { Form, Button, InputGroup, Image } from 'react-bootstrap';
+import { Form, Button, InputGroup, Image, Row, Col, Container } from 'react-bootstrap';
 import { BsSearch, BsChevronDown, BsChevronUp } from "react-icons/bs";
 import styles from '../assets/css/TableStyles.module.css';
 import logoMapper from '../assets/javascript/logoMapper.json';
@@ -47,58 +47,62 @@ function LeagueStandingsTable(props) {
     }
 
     return (
-        <Table responsive={true} hover id={styles.table}>
-            <thead>
-                <tr>
-                    <td colSpan="1">
+        <div>
+            <Container id={styles.actionForm}>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <p id={styles.actionFormLabel}>Filter By Season</p>
                         <Form.Control as="select" onChange={(e) => { fetchDataForTheSeason(e.target.value) }}>
                             {seasons.map(
                                 (season) => {
-                                    return <option value={season}>{season}</option>
+                                    return <option key={season} value={season}>{season}</option>
                                 }
                             )}
                         </Form.Control>
-                    </td>
-                    <td colSpan="8" />
-                    <td colSpan="2">
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <p id={styles.actionFormLabel}>Search for a Club</p>
                         <InputGroup className="mb-3">
                             <Form.Control type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => setSearchValue(e.target.value)} onKeyDown={(e) => handleKeyPress(e)} />
                             <InputGroup.Append>
                                 <Button variant="outline-info" onClick={() => { searchFunction(searchValue) }} style={{ backgroundColor: "#3d083e", color: "white", outline: "none" }}><BsSearch /></Button>
                             </InputGroup.Append>
                         </InputGroup>
-                    </td>
-                </tr>
-            </thead>
-            <thead id={styles.tableHeader}>
-                <tr>
-                    <th>Club</th><th>Position</th><th>MP</th><th>W</th><th>D</th><th>L</th><th>GF</th><th>GA</th><th>GD</th>
-                    <th onClick={() => sortFunction()}>Pts {props.isSorted ? <BsChevronDown /> : <BsChevronUp />}</th>
-                    <th>Last 5 Matches</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.data ? props.data.map(
-                    (club) => {
-                        return (
-                            <tr className={props.isSorted && !isSearching ? styles.descSorted : (!props.isSorted && !isSearching) ? styles.ascSorted : ""}>
-                                <td onClick={() => { handleModal(club) }} style={{cursor: "pointer"}}> <Image className={styles.clubLogo} fluid src={logoMapper[club.name]} alt="logo"/> {club.name}</td>
-                                <td>{club.rankings}</td>
-                                <td>{club.matches_played}</td>
-                                <td>{club.wins}</td>
-                                <td>{club.draws}</td>
-                                <td>{club.losses}</td>
-                                <td>{club.goals_for}</td>
-                                <td>{club.goals_against}</td>
-                                <td>{club.goal_difference}</td>
-                                <td>{club.points}</td>
-                                <td>{club.last_five_matches.map((m) => { return <span style={{ color: m === 'W' ? 'green' : m === 'L' ? 'red' : 'grey' }}> {m} </span> })}</td>
-                            </tr>
-                        )
-                    }
-                ) : ""}
-            </tbody>
-        </Table >
+                    </Col>
+                </Row>
+            </Container>
+
+            <Table responsive={true} hover id={styles.table}>
+                <thead id={styles.tableHeader}>
+                    <tr>
+                        <th>Position</th><th>Club</th><th>MP</th><th>W</th><th>D</th><th>L</th><th>GF</th><th>GA</th><th>GD</th>
+                        <th onClick={() => sortFunction()}>Pts {props.isSorted ? <BsChevronDown /> : <BsChevronUp />}</th>
+                        <th>Last 5 Matches</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.data ? props.data.map(
+                        (club) => {
+                            return (
+                                <tr key={club.name} className={props.isSorted && !isSearching ? styles.descSorted : (!props.isSorted && !isSearching) ? styles.ascSorted : ""}>
+                                    <td key={`${club.name}_rank`}>{club.rankings}</td>
+                                    <td key={`${club.name}_name`} onClick={() => { handleModal(club) }} style={{ cursor: "pointer" }}> <Image className={styles.clubLogo} fluid src={logoMapper[club.name]} alt="logo" /> {club.name}</td>
+                                    <td key={`${club.name}_played`}>{club.matches_played}</td>
+                                    <td key={`${club.name}_wins`}>{club.wins}</td>
+                                    <td key={`${club.name}_draws`}>{club.draws}</td>
+                                    <td key={`${club.name}_losses`}>{club.losses}</td>
+                                    <td key={`${club.name}_gf`}>{club.goals_for}</td>
+                                    <td key={`${club.name}_ga`}>{club.goals_against}</td>
+                                    <td key={`${club.name}_gd`}>{club.goal_difference}</td>
+                                    <td key={`${club.name}_pts`}>{club.points}</td>
+                                    <td key={`${club.name}_last5`}>{club.last_five_matches.map((m,i) => { return <span key={`${m}_${i}`}style={{ color: m === 'W' ? 'green' : m === 'L' ? 'red' : 'grey' }}> {m} </span> })}</td>
+                                </tr>
+                            )
+                        }
+                    ) : ""}
+                </tbody>
+            </Table >
+        </div>
     )
 }
 
