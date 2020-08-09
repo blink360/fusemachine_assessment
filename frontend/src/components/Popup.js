e7b39734b2e51b206b3a87596d20f2ba92ecc93a
Pranish Rajbhandari <rajbhandari.pranish@gmail.com>
2020-08-09 08:05:33 +0545

    Added favicon and updated webpack config. Added minor styling for the modal. Updated the title for the html page

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -9,20 +9,18 @@ function Popup(props) {
     return (
         <>
             <Modal show={props.showPopup} onHide={handleModalClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>{props.modalData.name}</Modal.Title>
+                <Modal.Header style={{backgroundColor:"#8b1359",color:"white"}} closeButton>
+                    <Modal.Title style={{width:"100%",textAlign:"center"}}>{props.modalData.name}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Row>
                         <Col xs={6}>
-                            <Image src={logoMapper[props.modalData.name]} alt="logo" style={{ width: "10em" }} />
+                            <Image src={logoMapper[props.modalData.name]} alt="logo" style={{ width: "10em", height:"10em" }} />
                         </Col>
                         <Col xs={6}>
-                            <p>Rank: {props.modalData.rankings} </p>
+                            <p>Season Rank: {props.modalData.rankings} </p>
                             <p>Matches Played: {props.modalData.matches_played}</p>
-                            <p>Wins: {props.modalData.wins}</p>
-                            <p>Losses: {props.modalData.losses}</p>
-                            <p>Draws: {props.modalData.draws}</p>
+                            <p>W/D/L: {props.modalData.wins}/{props.modalData.draws}/{props.modalData.losses}</p>
                             <p>Total Goals Scored: {props.modalData.goals_for}</p>
                         </Col>
                     </Row>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,25 @@
 var path = require('path');
-var HtmlWebpackPlugin =  require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry : ['./src/index.js','react-app-polyfill/stable'],
-    output : {
-        path : path.resolve(__dirname , 'dist'),
+    entry: ['./src/index.js', 'react-app-polyfill/stable'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'
     },
-    module : {
-        rules : [
-            {test : /\.(js)$/, use:'babel-loader'},
-            {test : /\.css$/, use:['style-loader', 'css-loader']},
-	    {test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,use:['url-loader?limit=100000']}
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/, use: ['url-loader?limit=100000'] }
         ]
     },
-    mode:'development',
-    plugins : [
-        new HtmlWebpackPlugin ({
-            template : './public/index.html'
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename:'index.html',
+            favicon: './src/favicon.png'
         })
     ]
 
